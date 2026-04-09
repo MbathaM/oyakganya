2aae4029354f874a732d8df0579093d09af3add9
MbathaM <33201125+MbathaM@users.noreply.github.com>
2026-04-09 18:08:17 +0200

    Update layout.tsx

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,18 +3,21 @@ import { SiteHeader } from '@/components/site-header';
 import { ReactNode } from 'react';
 import { TransitionComponent } from '@/components/transition';
 import ChatWidget from '@/components/chat-widget';
+import { CountdownBlocker } from '@/components/countdown-blocker';
 
 export default async function PagesLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <SiteHeader />
-      <main className="flex-1 relative">
-        <TransitionComponent>
-          {children}
-        </TransitionComponent>
-        <ChatWidget />
-        <SiteFooter />
-      </main>
-    </div>
+    <CountdownBlocker>
+      <div className="flex min-h-screen flex-col">
+        <SiteHeader />
+        <main className="flex-1 relative">
+          <TransitionComponent>
+            {children}
+          </TransitionComponent>
+          <ChatWidget />
+          <SiteFooter />
+        </main>
+      </div>
+    </CountdownBlocker>
   );
 }
